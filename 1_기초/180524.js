547a09fee1539d4b6acf8822347d73e975957d58
ChristianPark <poi3047@gmail.com>
2018-05-24 13:04:21 +0900

    18.05.24 basicJavaScriptTypes

---
+++ 1_기초/180524.js
@@ -0,0 +1,20 @@
+/*자료형과 변수*/
+
+/* 
+javaScript의 기본형
+숫자 = num
+문자열 = strting
+불리언 = boolean (true & false)
+
+*/
+var num= 5;
+console.log(num);
+
+var string = "안녕하세요. 저는 문자열입니다.";
+console.log(string);
+
+var boolean = true && false;
+console.log(boolean);
+
+var multiplication = 99*123;
+console.log(multiplication);
